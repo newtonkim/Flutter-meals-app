fa254ef290eba568f7c69eaf84a5650774a235cf
Newton <nkimathi@MacBook-Pro-van-Newton.local>
2023-07-30 11:11:59 +0300

    add second screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/categories_screen.dart';
-import 'package:meals/screens/meals.dart';
+
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,11 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      // home: const CategoriesScreen()// Todo ...,
-      home: const MealsScreen(
-        title: 'Some Categories....',
-        meals: [],
-      )// Todo ...,
+      home: const CategoriesScreen()// Todo ...,
     );
   }
 }
