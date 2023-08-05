3ab80a2d0c8e73485b4b1eb1692a672e762c7323
Admin <admin@Admins-iMac.local>
2023-08-05 15:55:28 +0300

    fix category title switching

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -6,11 +6,11 @@ import 'package:meals/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+     this.title,
     required this.meals,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
 //push to a new screen
@@ -56,10 +56,12 @@ class MealsScreen extends StatelessWidget {
         )
       );
     }
-
+      if(title == null){
+          content;
+      }
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
